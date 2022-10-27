ff22ced4fb0a44adf7df457cd1aac3862c4e9eeb
Yiğit Can Ayaz <54958108+YigitCanAyaz@users.noreply.github.com>
2022-10-27 15:05:19 +0300

    Code First integration for entity framework + validation added for customer

---
+++ FluentValidation.Web/FluentValidators/CustomerValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation.Web.Models;
+
+namespace FluentValidation.Web.FluentValidators
+{
+    public class CustomerValidator : AbstractValidator<Customer>
+    {
+        public CustomerValidator()
+        {
+            RuleFor(c => c.Name).NotEmpty().WithMessage("Name cannot be null!");
+            RuleFor(c => c.Email).NotEmpty().WithMessage("Email cannot be null!").EmailAddress().WithMessage("Email format is not correct!");
+            RuleFor(c => c.Age).NotEmpty().WithMessage("Age cannot be null!").InclusiveBetween(18, 60).WithMessage("Age should be between 18 and 60");
+        }
+    }
+}
---
+++ FluentValidation.Web/Models/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FluentValidation.Web.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        //[Required(ErrorMessage = "Name cannot be null!")]
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+    }
+}
