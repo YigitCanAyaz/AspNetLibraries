438a7f1bdf245c084efb0e28852f6fdfd01adf67
Yiğit Can Ayaz <54958108+YigitCanAyaz@users.noreply.github.com>
2022-10-28 22:13:42 +0300

    Projectin sample added to AutoMapper.Web Layer

---
+++ AutoMapper.Web/Controllers/ProjectionsController.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper.Web.DTOs;
+using AutoMapper.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutoMapper.Web.Controllers
+{
+    public class ProjectionsController : Controller
+    {
+        private readonly IMapper _mapper;
+
+        public ProjectionsController(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(EventDateDto eventDateDto)
+        {
+            EventDate eventDate = _mapper.Map<EventDate>(eventDateDto);
+
+            ViewBag.date = eventDate.Date.ToShortDateString();
+
+            return View();
+        }
+    }
+}
---
+++ AutoMapper.Web/Mapping/EventDateProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper.Web.DTOs;
+using AutoMapper.Web.Models;
+
+namespace AutoMapper.Web.Mapping
+{
+    public class EventDateProfile : Profile
+    {
+        public EventDateProfile()
+        {
+            CreateMap<EventDateDto, EventDate>().ForMember(e => e.Date, options => options.MapFrom(e => new DateTime(e.Year, e.Month, e.Day)));
+            CreateMap<EventDate, EventDateDto>()
+                .ForMember(e => e.Year, options => options.MapFrom(e => e.Date.Year))
+                .ForMember(e => e.Month, options => options.MapFrom(e => e.Date.Month))
+                .ForMember(e => e.Day, options => options.MapFrom(e => e.Date.Day));
+        }
+    }
+}
