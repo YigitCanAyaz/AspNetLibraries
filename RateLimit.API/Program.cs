8f0c21c01b128eeecd72fc30350f7248388f8017
Yiğit Can Ayaz <54958108+YigitCanAyaz@users.noreply.github.com>
2022-10-29 01:15:33 +0300

    Added RateLimit configuration + intergrated with controller for example

--- RateLimit.API/Program.cs
+++ RateLimit.API/Program.cs
@@ -1,11 +1,31 @@
+using AspNetCoreRateLimit;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddOptions(); // reading appsettings.json
+// reading cache service (takes request number to memory)
+builder.Services.AddMemoryCache();
+builder.Services.Configure<IpRateLimitOptions>(builder.Configuration.GetSection("IpRateLimiting"));
+builder.Services.Configure<IpRateLimitPolicies>(builder.Configuration.GetSection("IpRateLimitPolicies"));
+
+// adds reference
+builder.Services.AddSingleton<IIpPolicyStore, MemoryCacheIpPolicyStore>();
+// builder.Services.AddSingleton<IIpPolicyStore, DistributedCacheIpPolicyStore>(); => for multiple applications
+// adds reference (one reference in all application)
+builder.Services.AddSingleton<IRateLimitCounterStore, MemoryCacheRateLimitCounterStore>();
+
+// to read request data (header, ip vs.)
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+builder.Services.AddSingleton<IProcessingStrategy, AsyncKeyLockProcessingStrategy>();
+
+// main interface
+builder.Services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
 
 var app = builder.Build();
 
@@ -15,6 +35,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+// uses upper features and puts ip rate limit
+app.UseIpRateLimiting();
 
 app.UseHttpsRedirection();
 
