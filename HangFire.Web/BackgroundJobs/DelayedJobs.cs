898b35523f44f23bd280eaf32f425ddc8f25793e
Yiğit Can Ayaz <54958108+YigitCanAyaz@users.noreply.github.com>
2022-10-30 22:47:49 +0300

    Added DelayedJob to HangFire + real world example (adding text to image)

--- HangFire.Web/BackgroundJobs/DelayedJobs.cs
+++ HangFire.Web/BackgroundJobs/DelayedJobs.cs
@@ -1,6 +1,40 @@
-﻿namespace HangFire.Web.BackgroundJobs
+﻿using System.Drawing;
+
+namespace HangFire.Web.BackgroundJobs
 {
     public class DelayedJobs
     {
+        public static string AddWatermarkJob(string fileName, string watermarkText)
+        {
+            return Hangfire.BackgroundJob.Schedule(() => ApplyWatermark(fileName, watermarkText), TimeSpan.FromSeconds(20));
+        }
+        public static void ApplyWatermark(string fileName, string watermarkText)
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/pictures", fileName);
+
+            using (var bitmap = Bitmap.FromFile(path))
+            {
+                using (Bitmap tempBitmap = new Bitmap(bitmap.Width, bitmap.Height))
+                {
+                    using (Graphics graphics = Graphics.FromImage(tempBitmap))
+                    {
+                        graphics.DrawImage(bitmap, 0, 0);
+
+                        var font = new Font(FontFamily.GenericSansSerif, 25, FontStyle.Bold);
+
+                        var color = Color.FromArgb(255, 0, 0);
+
+                        var brush = new SolidBrush(color);
+
+                        // x, y
+                        var point = new Point(20, bitmap.Height - 50);
+
+                        graphics.DrawString(watermarkText, font, brush, point);
+
+                        tempBitmap.Save(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/pictures/watermarks", fileName));
+                    }
+                }
+            }
+        }
     }
 }
